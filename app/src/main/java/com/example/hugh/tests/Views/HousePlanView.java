8955eb3a2cb713629d950f6ce20706f3251600c5
黄帅 <huangshuai@zhujia360.com>
2018-11-07 18:32:47 +0800

    HousePlanView complete

--- app/src/main/java/com/example/hugh/tests/Views/HousePlanView.java
+++ app/src/main/java/com/example/hugh/tests/Views/HousePlanView.java
@@ -1,13 +1,18 @@
 package com.example.hugh.tests.Views;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Hugh on 2018/11/6.
@@ -16,10 +21,14 @@ import android.view.ViewGroup;
 
 public class HousePlanView extends ViewGroup {
 
+    private Context mContext;
     private int lastX;
     private int lastY;
     private Rect rect;
     private int mSelectedView = -1;
+    ArrayList<HashMap> mHomeTagList = new ArrayList<>();
+    final float TAG_WIDTH_SCALE = 6.0F;
+    final float TAG_HEIGHT_SCALE = 5.0F;
 
     public HousePlanView(Context context) {
         super(context);
@@ -27,21 +36,12 @@ public class HousePlanView extends ViewGroup {
 
     public HousePlanView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.mContext = context;
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        int mTotalHeight = 0;
-        int childCount = getChildCount();
-        for (int i = 0; i < childCount; i++) {
-            View childView = getChildAt(i);
-            int measureHeight = childView.getMeasuredHeight();
-            int measuredWidth = childView.getMeasuredWidth();
 
-            childView.layout(l, mTotalHeight, measuredWidth, mTotalHeight
-                    + measureHeight);
-            mTotalHeight += measureHeight;
-        }
     }
 
     @Override
@@ -81,6 +81,7 @@ public class HousePlanView extends ViewGroup {
                     Rect deleteRect = new Rect(child.getLeft()+child.getWidth()-50, child.getTop()+50, child.getRight(), child.getBottom());
                     if (rect.contains(x1,y1)){
                         mSelectedView = i;
+                        child.bringToFront();
                     }
                     if (deleteRect.contains(x1,y1)){
                         removeViewAt(i);
@@ -99,12 +100,14 @@ public class HousePlanView extends ViewGroup {
                 if (mSelectedView != -1){
                     int offsetX = x - lastX;
                     int offsetY = y - lastY;
-                    Rect rect1 = new Rect(getChildAt(mSelectedView).getLeft() + offsetX, getChildAt(mSelectedView).getTop() + offsetY,
+                    Rect rect1 = new Rect(getChildAt(mSelectedView).getLeft() + offsetX,
+                            getChildAt(mSelectedView).getTop() + offsetY,
                             getChildAt(mSelectedView).getRight() + offsetX,
                             getChildAt(mSelectedView).getBottom() + offsetY);
                     Log.e("size",rect.toString()+"-|-"+rect1.toString());
                     if (this.rect.contains(rect1)){
-                        getChildAt(mSelectedView).layout(
+                        View childAt = getChildAt(mSelectedView);
+                        childAt.layout(
                                 getChildAt(mSelectedView).getLeft() + offsetX,
                                 getChildAt(mSelectedView).getTop() + offsetY,
                                 getChildAt(mSelectedView).getRight() + offsetX,
@@ -121,7 +124,59 @@ public class HousePlanView extends ViewGroup {
         return true;
     }
 
-    public void addRoom(String name,float positionX,float positionY){
-        
+    public void addRoom(String name,double xScale,double yScale){
+        TextView textView = new TextView(mContext);
+        int height = getHeight();
+        int width = getWidth();
+        int childWidth = (int) (getWidth() / TAG_WIDTH_SCALE);
+        int childHeight = (int) (getHeight() / TAG_HEIGHT_SCALE);
+        LayoutParams layoutParams = new LayoutParams(childWidth, childHeight);
+        textView.setLayoutParams(layoutParams);
+        textView.setGravity(Gravity.CENTER);
+        textView.setBackgroundColor(Color.CYAN);
+        textView.setText(name);
+        if (xScale == 0 && yScale == 0){
+            textView.layout(width/2-childWidth/2,height/2-childHeight/2,width/2+childWidth/2,height/2+childHeight/2);
+        }else {
+            int left = (int) (width * xScale - childWidth / 2);
+            int top = (int) (height * yScale - childHeight / 2);
+            int right = (int) (width * xScale + childWidth / 2);
+            int bottom = (int) (height * yScale + childHeight / 2);
+            textView.layout(left,top,right,bottom);
+        }
+        addView(textView);
+    }
+
+    @Override
+    public void onViewAdded(View child) {
+        super.onViewAdded(child);
+
+    }
+
+    public void saveRoomTags(){
+        mHomeTagList.clear();
+        int childCount = getChildCount();
+        for (int i=0;i<childCount;i++){
+            View childAt = getChildAt(i);
+            int left = childAt.getLeft();
+            int top = childAt.getTop();
+            String roomName = ((TextView) childAt).getText().toString();
+            int height = childAt.getHeight();
+            int width = childAt.getWidth();
+            double x = left + width / 2;
+            double y = top + height / 2;
+            double scaleX = x/getWidth();
+            double scaleY = y/getHeight();
+            DecimalFormat decimalFormat = new DecimalFormat("0.0000");
+            String XFormat = decimalFormat.format(scaleX);
+            String YFormat = decimalFormat.format(scaleY);
+            Log.e("result",x+","+y+","+XFormat+","+YFormat+"-"+roomName);
+            HashMap<String, String> tagMap = new HashMap<>();
+            tagMap.put("roomName",roomName);
+            tagMap.put("xScale",XFormat);
+            tagMap.put("yScale",YFormat);
+            mHomeTagList.add(tagMap);
+        }
+        Log.e("result",mHomeTagList.toString());
     }
 }
--- app/src/main/java/com/example/hugh/tests/activity/ScrollActivity.java
+++ app/src/main/java/com/example/hugh/tests/activity/ScrollActivity.java
@@ -2,8 +2,11 @@ package com.example.hugh.tests.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.hugh.tests.R;
+import com.example.hugh.tests.Views.HousePlanView;
 
 public class ScrollActivity extends AppCompatActivity {
 
@@ -11,5 +14,37 @@ public class ScrollActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll);
+        final HousePlanView hpv = findViewById(R.id.hpv);
+        Button bedroom = findViewById(R.id.bedroom);
+        Button living_room = findViewById(R.id.living_room);
+        Button save = findViewById(R.id.save);
+        bedroom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hpv.addRoom("卧室",0.6287,0.2783);
+            }
+        });
+
+        living_room.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hpv.addRoom("客厅",0.2361,0.3550);
+                hpv.addRoom("客厅",0.3213,0.6983);
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hpv.saveRoomTags();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
     }
 }
