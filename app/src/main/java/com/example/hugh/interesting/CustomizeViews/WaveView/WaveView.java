7de91d3efa6319d6fbd14747fbf0ce1fcddb9244
Hugh <huangshuai@zhujia360.com>
2021-01-28 14:51:07 +0800

    cpb

--- app/src/main/java/com/example/hugh/interesting/CustomizeViews/WaveView/WaveView.java
+++ app/src/main/java/com/example/hugh/interesting/CustomizeViews/WaveView/WaveView.java
@@ -1,5 +1,6 @@
 package com.example.hugh.interesting.CustomizeViews.WaveView;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -8,23 +9,58 @@ import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 
 
 public class WaveView extends View {
+
+    private Path path;
+    private Paint paint;
+    int mItemWaveLength = 1200;
+    private int dx;
+
     public WaveView(Context context) {
         super(context);
     }
 
     public WaveView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        Path path = new Path();
-        Paint paint = new Paint();
+        path = new Path();
+        paint = new Paint();
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.FILL);
+        startAnim();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        path.reset();
+        int originY = 300;
+        int halfWaveLen = mItemWaveLength >> 1;
+        path.moveTo(-mItemWaveLength + dx, originY);
+        for (int i = -mItemWaveLength; i <= getWidth() + mItemWaveLength; i += mItemWaveLength) {
+            path.rQuadTo(halfWaveLen >> 1, -100, halfWaveLen, 0);
+            path.rQuadTo(halfWaveLen >> 1, 100, halfWaveLen, 0);
+        }
+        path.lineTo(getWidth(),getHeight());
+        path.lineTo(0,getHeight());
+        path.close();
+        canvas.drawPath(path, paint);
+    }
+
+    public void startAnim() {
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, mItemWaveLength);
+        valueAnimator.setDuration(2000);
+        valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        valueAnimator.setInterpolator(new LinearInterpolator());
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                dx = (int) animation.getAnimatedValue();
+                postInvalidate();
+            }
+        });
+        valueAnimator.start();
     }
 }
